4664cc3a3f50cd94727695b998ca336a81245c92
ozzyfromspace <float.circuit@gmail.com>
2023-03-16 02:54:25 -0400

    add nontrivial test routes

--- api/index.go
+++ api/index.go
@@ -2,7 +2,12 @@ package handler
 
 import (
 	"net/http"
+	"tilla/apis"
+	"tilla/models"
+	"time"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,53 +15,54 @@ import (
 // 	fmt.Fprintf(w, "Hello World from Go!")
 // }
 
-func Go(w http.ResponseWriter, r *http.Request) {
-	app := gin.Default()
+// func Go(w http.ResponseWriter, r *http.Request) {
+// 	app := gin.Default()
+//
+// 	app.GET("/api/test", func(ctx *gin.Context) {
+// 		ctx.JSON(http.StatusOK, gin.H{"msg": "test working..."})
+// 	})
+//
+// 	app.ServeHTTP(w, r)
+// }
 
-	app.GET("/api/test", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"msg": "test working..."})
+func Handler(w http.ResponseWriter, r *http.Request) {
+	db := models.NewDatabase()
+
+	app := gin.Default()
+	app.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	app.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"msg": "welcome to the eclipse academy API"})
 	})
 
+	apiGroup := app.Group("/api")
+
+	apis.NewStudentApi(apiGroup, db).RegisterApi()
+	apis.NewTeacherApi(apiGroup, db).RegisterApi()
+	apis.NewCalendarApi(apiGroup, db).RegisterApi()
+
+	app.NoRoute(func(c *gin.Context) {
+		payload := gin.H{
+			"msg":  "route not implemented",
+			"path": c.Request.URL.Path,
+		}
+		// c.JSON(http.StatusNotImplemented, models.MsgPayload("route not implemented"))
+		c.JSON(http.StatusNotImplemented, payload)
+	})
+
+	// apiGroup.Run(":8080")
 	app.ServeHTTP(w, r)
 }
 
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// 	db := models.NewDatabase()
-//
-// 	app := gin.Default()
-// 	app.Use(gzip.Gzip(gzip.DefaultCompression))
-//
-// 	app.Use(cors.New(cors.Config{
-// 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-// 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
-// 		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
-// 		AllowCredentials: true,
-// 		AllowOriginFunc: func(origin string) bool {
-// 			return true
-// 		},
-// 		MaxAge: 12 * time.Hour,
-// 	}))
-//
-// 	app.GET("/", func(c *gin.Context) {
-// 		c.JSON(http.StatusOK, gin.H{"msg": "welcome to the eclipse academy API"})
-// 	})
-//
-// 	apiGroup := app.Group("/api")
-//
-// 	apis.NewStudentApi(apiGroup, db).RegisterApi()
-// 	apis.NewTeacherApi(apiGroup, db).RegisterApi()
-// 	apis.NewCalendarApi(apiGroup, db).RegisterApi()
-//
-// 	app.NoRoute(func(c *gin.Context) {
-// 		payload := gin.H{
-// 			"msg":  "route not implemented",
-// 			"path": c.Request.URL.Path,
-// 		}
-// 		// c.JSON(http.StatusNotImplemented, models.MsgPayload("route not implemented"))
-// 		c.JSON(http.StatusNotImplemented, payload)
-// 	})
-//
-// 	// apiGroup.Run(":8080")
-// 	app.ServeHTTP(w, r)
-// }
 //
