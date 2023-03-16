0fc9e465e89801ed89afcc6aa819726d1a54f595
ozzyfromspace <float.circuit@gmail.com>
2023-03-16 01:43:42 -0400

    work with test route

--- api/index.go
+++ api/index.go
@@ -2,12 +2,7 @@ package handler
 
 import (
 	"net/http"
-	"tilla/apis"
-	"tilla/models"
-	"time"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,38 +10,53 @@ import (
 // 	fmt.Fprintf(w, "Hello World from Go!")
 // }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	db := models.NewDatabase()
-
+func Go(w http.ResponseWriter, r *http.Request) {
 	app := gin.Default()
-	app.Use(gzip.Gzip(gzip.DefaultCompression))
 
-	app.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
-		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-		MaxAge: 12 * time.Hour,
-	}))
-
-	apiGroup := app.Group("/api")
-
-	apis.NewStudentApi(apiGroup, db).RegisterApi()
-	apis.NewTeacherApi(apiGroup, db).RegisterApi()
-	apis.NewCalendarApi(apiGroup, db).RegisterApi()
-
-	app.NoRoute(func(c *gin.Context) {
-		payload := gin.H{
-			"msg":  "route not implemented",
-			"path": c.Request.URL.Path,
-		}
-		// c.JSON(http.StatusNotImplemented, models.MsgPayload("route not implemented"))
-		c.JSON(http.StatusNotImplemented, payload)
+	app.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"msg": "test working..."})
 	})
 
-	// apiGroup.Run(":8080")
 	app.ServeHTTP(w, r)
 }
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	db := models.NewDatabase()
+//
+// 	app := gin.Default()
+// 	app.Use(gzip.Gzip(gzip.DefaultCompression))
+//
+// 	app.Use(cors.New(cors.Config{
+// 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+// 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+// 		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+// 		AllowCredentials: true,
+// 		AllowOriginFunc: func(origin string) bool {
+// 			return true
+// 		},
+// 		MaxAge: 12 * time.Hour,
+// 	}))
+//
+// 	app.GET("/", func(c *gin.Context) {
+// 		c.JSON(http.StatusOK, gin.H{"msg": "welcome to the eclipse academy API"})
+// 	})
+//
+// 	apiGroup := app.Group("/api")
+//
+// 	apis.NewStudentApi(apiGroup, db).RegisterApi()
+// 	apis.NewTeacherApi(apiGroup, db).RegisterApi()
+// 	apis.NewCalendarApi(apiGroup, db).RegisterApi()
+//
+// 	app.NoRoute(func(c *gin.Context) {
+// 		payload := gin.H{
+// 			"msg":  "route not implemented",
+// 			"path": c.Request.URL.Path,
+// 		}
+// 		// c.JSON(http.StatusNotImplemented, models.MsgPayload("route not implemented"))
+// 		c.JSON(http.StatusNotImplemented, payload)
+// 	})
+//
+// 	// apiGroup.Run(":8080")
+// 	app.ServeHTTP(w, r)
+// }
+//
