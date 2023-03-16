3581ed129d6c1d28ef22c9e27cbdeb9d4aca5ae4
ozzyfromspace <float.circuit@gmail.com>
2023-03-15 23:55:10 -0400

    attempt to expose go api in vercel

---
+++ api/index.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"net/http"
+	"tilla/apis"
+	"tilla/models"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	db := models.NewDatabase()
+
+	app := gin.Default()
+	app.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	app.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	apis.NewStudentApi(app, db).RegisterApi()
+	apis.NewTeacherApi(app, db).RegisterApi()
+	apis.NewCalendarApi(app, db).RegisterApi()
+
+	app.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotImplemented, models.MsgPayload("route not implemented"))
+	})
+
+	// app.Run(":8080")
+	app.ServeHTTP(w, r)
+}
